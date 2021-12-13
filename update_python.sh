319bc53ec176dfd5d63f5196c9c050c20a239835
Kimio Kuramitsu <kkuramitsu@gmail.com>
2021-12-13 11:49:08 +0900

    Update update_python.sh

--- update_python.sh
+++ update_python.sh
@@ -12,4 +12,16 @@ python3.9 -m pip install --upgrade poetry
 #sudo update-alternatives --set python /usr/bin/python3.9
 #sudo ln -sf /usr/bin/python /usr/local/bin/python
 
+# サンキュー 小松
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.9 2
+sudo update-alternatives --config python
+
 python --version
+
+# t5-japanese を持ってくる
+git clone https://github.com/megagonlabs/t5-japanese.git
+
+# teruch5
+cd t5-japanese
+!poetry install
