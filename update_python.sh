56d8cb660063edb9b90d42cc8cb15694b687744e
Kimio Kuramitsu <kkuramitsu@gmail.com>
2021-12-13 11:18:46 +0900

    Create update_python.sh

---
+++ update_python.sh
@@ -0,0 +1,13 @@
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt-get -y update
+sudo apt-get -y install python3.9
+sudo apt-get -y install python3.9-dev
+sudo apt-get -y install python3-pip
+sudo apt-get -y install python3.9-distutils
+python3.9 -m pip install --upgrade setuptools
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install --upgrade distlib
+sudo update-alternatives --set python /usr/bin/python3.9
+sudo ln -sf /usr/bin/python /usr/local/bin/python
+
+python --version
